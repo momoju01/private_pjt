36084fe665e1ebc10e60491ae66f49f64ba625ff
김윤하 <beupthere@gmail.com>
2021-09-17 17:50:58 +0900

    pjt06

---
+++ pjt06/community/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_safe, require_POST, require_http_methods
+from django.contrib.auth.decorators import login_required
+from .models import Review
+from .forms import ReviewForm
+
+# Create your views here.
+@require_safe
+def index(request):
+    reviews = Review.objects.order_by('-pk')
+    context = {
+        'reviews': reviews,
+    }
+    return render(request, 'community/index.html', context)
+
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def create(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save()
+            return redirect('community:detail', review.pk)
+    else:
+        form = ReviewForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'community/create.html', context)
+
+
+@require_safe
+def detail(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    context = {
+        'review': review,
+    }
+    return render(request, 'community/detail.html', context)
+
+
+@require_POST
+def delete(request, pk):
+    if request.user.is_authenticated:
+        review = get_object_or_404(Review, pk=pk)
+        review.delete()
+    return redirect('community:index')
+
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def update(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            return redirect('community:detail', review.pk)
+    else:
+        form = ReviewForm(instance=review)
+    context = {
+        'review': review,
+        'form': form,
+    }
+    return render(request, 'community/update.html', context)
